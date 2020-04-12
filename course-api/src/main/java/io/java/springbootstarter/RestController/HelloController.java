e796c246a3437625c7d85bc4170d6607d964d9c8
Deepak <Deepak@Deepak>
2020-04-13 01:19:53 +0530

    Initial Commit

---
+++ course-api/src/main/java/io/java/springbootstarter/RestController/HelloController.java
@@ -0,0 +1,16 @@
+package io.java.springbootstarter.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@RequestMapping("/hello")
+	public String say() {
+		return "hi";
+	}
+
+
+}
